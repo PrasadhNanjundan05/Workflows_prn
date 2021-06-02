75fad6a9cee0ca8f3f31bcb555b26bd1975d6bb7
jboss <jboss@cd579f311e85>
2021-06-02 07:48:31 +0000

    Updating Workflow on  - 02-06-2021 07:48:31 - ncroot

--- Fast_Network_Scan/Process_Network_Scan/Tasks/.meta_Task_network_scan.py
+++ Fast_Network_Scan/Process_Network_Scan/Tasks/.meta_Task_network_scan.py
@@ -7,7 +7,7 @@
         </entry>
         <entry>
             <key>DATE_MODIFICATION</key>
-            <value>1622619861869</value>
+            <value>1622620111816</value>
         </entry>
         <entry>
             <key>REPOSITORY</key>
@@ -15,7 +15,7 @@
         </entry>
         <entry>
             <key>DATE_CREATION</key>
-            <value>1622619861853</value>
+            <value>1622620111794</value>
         </entry>
         <entry>
             <key>TAG</key>
--- Fast_Network_Scan/Process_Network_Scan/Tasks/Task_network_scan.py
+++ Fast_Network_Scan/Process_Network_Scan/Tasks/Task_network_scan.py
@@ -5,7 +5,7 @@ from msa_sdk.msa_api import MSA_API
 dev_var = Variables()
 dev_var.add('network', var_type='String')
 
-
+context = Variables.task_call(dev_var)
 
 ret = MSA_API.process_content('ENDED', 'Task OK', context, True)
 print(ret)
