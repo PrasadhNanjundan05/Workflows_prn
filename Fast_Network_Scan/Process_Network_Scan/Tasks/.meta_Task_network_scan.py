f58a4e1b5a439f16c16ea15b4d18e7bf4ccd7462
jboss <jboss@cd579f311e85>
2021-06-02 08:06:32 +0000

    Updating Workflow on  - 02-06-2021 08:06:32 - ncroot

--- Fast_Network_Scan/Process_Network_Scan/Tasks/.meta_Task_network_scan.py
+++ Fast_Network_Scan/Process_Network_Scan/Tasks/.meta_Task_network_scan.py
@@ -7,7 +7,7 @@
         </entry>
         <entry>
             <key>DATE_MODIFICATION</key>
-            <value>1622620111816</value>
+            <value>1622621192611</value>
         </entry>
         <entry>
             <key>REPOSITORY</key>
@@ -15,7 +15,7 @@
         </entry>
         <entry>
             <key>DATE_CREATION</key>
-            <value>1622620111794</value>
+            <value>1622621192601</value>
         </entry>
         <entry>
             <key>TAG</key>
--- Fast_Network_Scan/Process_Network_Scan/Tasks/Task_network_scan.py
+++ Fast_Network_Scan/Process_Network_Scan/Tasks/Task_network_scan.py
@@ -1,11 +1,18 @@
 from msa_sdk.variables import Variables
 from msa_sdk.msa_api import MSA_API
-
+import nmap
 
 dev_var = Variables()
 dev_var.add('network', var_type='String')
 
 context = Variables.task_call(dev_var)
+
+nm = nmap.PortScanner()
+nm.scan(hosts=context['network'], arguments='-n -sP')
+hosts_list = [(x, nm[x]['status']['state']) for x in nm.all_hosts()]
+
+for host, status in hosts_list:
+    context[host] = status
 
 ret = MSA_API.process_content('ENDED', 'Task OK', context, True)
 print(ret)
