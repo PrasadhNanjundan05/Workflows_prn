98d1aeb68dc553286735903d38d442a779ceaca7
jboss <jboss@978997f5f38b>
2021-06-16 10:34:49 +0000

    Updating Workflow on  - 16-06-2021 10:34:48 - ncroot

--- Fast_Network_Scan/Process_Network_Scan/Tasks/.meta_Task_network_scan.py
+++ Fast_Network_Scan/Process_Network_Scan/Tasks/.meta_Task_network_scan.py
@@ -7,7 +7,7 @@
         </entry>
         <entry>
             <key>DATE_MODIFICATION</key>
-            <value>1622629596429</value>
+            <value>1623839688966</value>
         </entry>
         <entry>
             <key>REPOSITORY</key>
@@ -15,7 +15,7 @@
         </entry>
         <entry>
             <key>DATE_CREATION</key>
-            <value>1622629596423</value>
+            <value>1623839688958</value>
         </entry>
         <entry>
             <key>TAG</key>
--- Fast_Network_Scan/Process_Network_Scan/Tasks/Task_network_scan.py
+++ Fast_Network_Scan/Process_Network_Scan/Tasks/Task_network_scan.py
@@ -1,24 +1,52 @@
 from msa_sdk.variables import Variables
 from msa_sdk.msa_api import MSA_API
-import nmap
+from msa_sdk.orchestration import Orchestration
+import subprocess
+import re
+import time
 
 dev_var = Variables()
 dev_var.add('network', var_type='String')
 
 context = Variables.task_call(dev_var)
+network = context['network']
+context['hosts'] = dict()
 
-nm = nmap.PortScanner()
-nm.scan(hosts=context['network'], arguments='-n -sP')
-hosts_list = [(x, nm[x]['status']['state']) for x in nm.all_hosts()]
+Orchestration = Orchestration(context['UBIQUBEID'])
+async_update_list = (context['PROCESSINSTANCEID'],
+                     context['TASKID'], context['EXECNUMBER'])
+status_message = ''
 i = 0
-for host, status in hosts_list:
-    if i == 0 :
-      context['hosts'] = dict()
-    context['hosts'][i] = dict()
-    context['hosts'][i]['host'] = host
-    context['hosts'][i]['status'] = status
-    i += 1
+# throw message
+def update_info(no_host_found: int, status_message: str):
+    message = "no of host found: "+ str(no_host_found) +" "
+    if status_message:
+        message += status_message
+    Orchestration.update_asynchronous_task_details(*async_update_list, message)
+    
+update_info(i, status_message)
+
+process = subprocess.Popen(['nmap', '-n', '-sn', '-vvv', network], stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
+for line in process.stdout:
+    update_message = False
+    result = re.match('^Nmap\sscan\sreport\sfor\s(\S+)$', line.decode())
+    if result:
+        context['hosts'][i] = dict()
+        context['hosts'][i]['ip_address'] = result.group(1)
+        update_message = True
+        i += 1 
+    else:
+        result = re.match('^Ping\sScan\sTiming:\s(.*)', line.decode())
+        if result:
+            status_message = result.group(1)
+            update_message = True
+    if update_message:
+        update_info(i, status_message) 
+
+time.sleep(1)        
+update_info(i, status_message)
+time.sleep(1)
+            
 
 ret = MSA_API.process_content('ENDED', 'Task OK', context, True)
 print(ret)
-
