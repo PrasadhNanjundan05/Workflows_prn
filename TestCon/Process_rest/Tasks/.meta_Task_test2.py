84433e5264a801f8280416cd9ffb4154a746c838
jboss <jboss@28751b7f796e>
2021-06-17 12:45:28 +0000

    Updating Workflow on  - 17-06-2021 12:45:28 - ncroot

--- TestCon/Process_rest/Tasks/.meta_Task_test2.py
+++ TestCon/Process_rest/Tasks/.meta_Task_test2.py
@@ -7,7 +7,7 @@
         </entry>
         <entry>
             <key>DATE_MODIFICATION</key>
-            <value>1623933921936</value>
+            <value>1623933928662</value>
         </entry>
         <entry>
             <key>REPOSITORY</key>
@@ -15,7 +15,7 @@
         </entry>
         <entry>
             <key>DATE_CREATION</key>
-            <value>1623933921933</value>
+            <value>1623933928640</value>
         </entry>
         <entry>
             <key>TAG</key>
