78bb72b23fc387a7a27fa0d4056785f42d0da5d1
jboss <jboss@17c678dfb6a0>
2021-06-23 06:55:50 +0000

    Updating Workflow on  - 23-06-2021 06:55:49 - ncroot

--- Fast_Network_Scan/Process_Review_and_Launch_Deep_Scan/Tasks/.meta_Task_launch_deep_scan.py
+++ Fast_Network_Scan/Process_Review_and_Launch_Deep_Scan/Tasks/.meta_Task_launch_deep_scan.py
@@ -7,7 +7,7 @@
         </entry>
         <entry>
             <key>DATE_MODIFICATION</key>
-            <value>1624017449792</value>
+            <value>1624431349857</value>
         </entry>
         <entry>
             <key>REPOSITORY</key>
@@ -15,7 +15,7 @@
         </entry>
         <entry>
             <key>DATE_CREATION</key>
-            <value>1624017449771</value>
+            <value>1624431349839</value>
         </entry>
         <entry>
             <key>TAG</key>
--- Fast_Network_Scan/Process_Review_and_Launch_Deep_Scan/Tasks/Task_launch_deep_scan.py
+++ Fast_Network_Scan/Process_Review_and_Launch_Deep_Scan/Tasks/Task_launch_deep_scan.py
@@ -3,42 +3,70 @@ from msa_sdk.msa_api import MSA_API
 from msa_sdk.orchestration import Orchestration
 from msa_sdk import util
 import json
+import time
 
 dev_var = Variables()
-dev_var.add('hostsk.0.ip_address', var_type='Ip Address')
-dev_var.add('hostsk.0.selected', var_type='Boolean')
+dev_var.add('hosts.0.ip_address', var_type='Ip Address')
+dev_var.add('hosts.0.selected', var_type='Boolean')
 dev_var.add('snmp_communities.0.name', var_type='String')
 context = Variables.task_call(dev_var)
 
 Orchestration = Orchestration(context['UBIQUBEID'])
 
-hosts = context['hostsk']
+hosts = context['hosts']
 communities = context['snmp_communities']
 
-selected_hosts = dict()
-i = 0
+selected_hosts = list()
+
 for host in hosts:
     selected = bool(host['selected'])
     if selected:
-        selected_hosts[i] = dict()
-        selected_hosts[i]['ip_address'] = host['ip_address']
-        i += 1
+        selected_host = dict()
+        selected_host['ip_address'] = host['ip_address']
+        selected_hosts.append(selected_host)
         
 hosts_per_wf = []
+process_ids = dict()
 
-for key, value in selected_hosts.items():
+for host in selected_hosts:
     hosts_per = dict()
-    hosts_per['ip_address'] = value['ip_address']
+    hosts_per['ip_address'] = host['ip_address']
     hosts_per_wf.append(hosts_per)
     if len(hosts_per_wf) == 2:
         data = dict()
         data['hosts'] = dict()
         data['hosts'] = hosts_per_wf
         data['snmp_communities'] = communities
-        util.log_to_process_file(context['SERVICEINSTANCEID'], json.dumps(data), context['PROCESSINSTANCEID'])
-        Orchestration.execute_service('Process/workflows/Deep_Network_Scan/Deep_Network_Scan', 'Process/workflows/Deep_Network_Scan/Process_Deep_Network_Scan', data)
+        serviceId, processId = Orchestration.execute_service_process('Process/workflows/Deep_Network_Scan/Deep_Network_Scan', 'Process/workflows/Deep_Network_Scan/Process_Deep_Network_Scan', data)
+        if processId and serviceId is not None:
+            process_info = dict()
+            process_info[processId] = serviceId
+            process_ids.update(process_info);
         hosts_per_wf = []
-    
+
+while bool(process_ids):
+    for k, v in list(process_ids.items()):
+        process_status = Orchestration.get_process_status_by_id(k)
+        if process_status == 'RUNNING':
+            time.sleep(5)
+        elif process_status == 'ENDED':
+            process_ids.pop(k)
+            Orchestration.get_service_variables(v)
+            hosts = list(filter(lambda var: var['name'] == 'hosts', json.loads(Orchestration.content)))
+            hosts = list(hosts[0]['value'].values())
+            for host in hosts:
+                context['hosts'] = list(filter(lambda i: i['ip_address'] != host['ip_address'], context['hosts']))
+                real_host = dict()
+                real_host['ip_address'] = host['ip_address']
+                real_host['selected'] = True
+                real_host['vendor'] = host['vendor']
+                real_host['model'] = host['model']
+                context['hosts'].append(real_host)
+            context['hosts'] = sorted(context['hosts'], key=lambda k: k['ip_address']) 
+        elif process_status == 'FAIL':
+            process_ids.pop(k)
+        else:
+            continue
 
 
 ret = MSA_API.process_content('ENDED', 'Task OK', context, True)
