739edb3a62c63f108cda394a3a0080f4009b5382
jboss <jboss@17c678dfb6a0>
2021-06-24 11:53:30 +0000

    Updating Workflow on  - 24-06-2021 11:53:30 - ncroot

--- Fast_Network_Scan/Process_Network_Scan/Tasks/.meta_Task_network_scan.py
+++ Fast_Network_Scan/Process_Network_Scan/Tasks/.meta_Task_network_scan.py
@@ -7,7 +7,7 @@
         </entry>
         <entry>
             <key>DATE_MODIFICATION</key>
-            <value>1623935272194</value>
+            <value>1624535610356</value>
         </entry>
         <entry>
             <key>REPOSITORY</key>
@@ -15,7 +15,7 @@
         </entry>
         <entry>
             <key>DATE_CREATION</key>
-            <value>1623935272176</value>
+            <value>1624535610352</value>
         </entry>
         <entry>
             <key>TAG</key>
--- Fast_Network_Scan/Process_Network_Scan/Tasks/Task_network_scan.py
+++ Fast_Network_Scan/Process_Network_Scan/Tasks/Task_network_scan.py
@@ -10,14 +10,13 @@ dev_var.add('network', var_type='String')
 
 context = Variables.task_call(dev_var)
 network = context['network']
-context['hosts'] = dict()
+context['hosts'] = list()
 
 Orchestration = Orchestration(context['UBIQUBEID'])
 async_update_list = (context['PROCESSINSTANCEID'],
                      context['TASKID'], context['EXECNUMBER'])
 status_message = ''
 i = 0
-# throw message
 def update_info(no_host_found: int, status_message: str):
     message = "no of host found: "+ str(no_host_found) +" "
     if status_message:
@@ -31,11 +30,14 @@ for line in process.stdout:
     update_message = False
     result = re.match('^Nmap\sscan\sreport\sfor\s(\S+)$', line.decode())
     if result:
-        context['hosts'][i] = dict()
-        context['hosts'][i]['ip_address'] = result.group(1)
-        context['hosts'][i]['selected'] = False
+        host_info = dict()
+        host_info['ip_address'] = result.group(1)
+        host_info['selected'] = True
+        host_info['vendor'] = None
+        host_info['model'] = None
+        context['hosts'].append(host_info)
         update_message = True
-        i += 1 
+        i += 1
     else:
         result = re.match('^Ping\sScan\sTiming:\s(.*)', line.decode())
         if result:
@@ -44,10 +46,5 @@ for line in process.stdout:
     if update_message:
         update_info(i, status_message) 
 
-time.sleep(1)        
-update_info(i, status_message)
-time.sleep(1)
-            
-
-ret = MSA_API.process_content('ENDED', 'Task OK', context, True)
+ret = MSA_API.process_content('ENDED', 'Task OK : '+str(i)+' hosts found', context, True)
 print(ret)
