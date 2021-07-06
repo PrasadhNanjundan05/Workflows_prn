d4f2f308dee95e6d9ff424ece213f47ce5a07acf
PrasadhNanjundan05 <>
2021-07-06 10:37:07 +0000

    Updating Workflow on  - 06-07-2021 10:37:07 - ncroot

--- Fast_Network_Scan/Process_AWS_discovery/Tasks/.meta_Task_Create_AWS_Device.py
+++ Fast_Network_Scan/Process_AWS_discovery/Tasks/.meta_Task_Create_AWS_Device.py
@@ -7,7 +7,7 @@
         </entry>
         <entry>
             <key>DATE_MODIFICATION</key>
-            <value>1625565897521</value>
+            <value>1625567827456</value>
         </entry>
         <entry>
             <key>REPOSITORY</key>
@@ -15,7 +15,7 @@
         </entry>
         <entry>
             <key>DATE_CREATION</key>
-            <value>1625565897504</value>
+            <value>1625567827439</value>
         </entry>
         <entry>
             <key>TAG</key>
