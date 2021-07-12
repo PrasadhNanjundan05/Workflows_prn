eea68b14865e204885aea8a90b578a9a38901296
PrasadhNanjundan05 <>
2021-07-12 12:00:12 +0000

    Updating Workflow on  - 12-07-2021 12:00:12 - ncroot

--- Upgrade_MSA_/Process_Pull_Latest_Config/Tasks/.meta_Task_pullconfig.py
+++ Upgrade_MSA_/Process_Pull_Latest_Config/Tasks/.meta_Task_pullconfig.py
@@ -7,7 +7,7 @@
         </entry>
         <entry>
             <key>DATE_MODIFICATION</key>
-            <value>1626091192674</value>
+            <value>1626091212640</value>
         </entry>
         <entry>
             <key>REPOSITORY</key>
@@ -15,7 +15,7 @@
         </entry>
         <entry>
             <key>DATE_CREATION</key>
-            <value>1626091192670</value>
+            <value>1626091212637</value>
         </entry>
         <entry>
             <key>TAG</key>
