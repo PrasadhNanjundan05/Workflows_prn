ba3802c0b35958429b1ecbbe6c0b469e2f466bc4
PrasadhNanjundan05 <>
2021-07-12 13:33:47 +0000

    Updating Workflow on  - 12-07-2021 13:33:46 - ncroot

--- Upgrade_MSA_/Process_Backup/Tasks/.meta_Task_backup.py
+++ Upgrade_MSA_/Process_Backup/Tasks/.meta_Task_backup.py
@@ -7,7 +7,7 @@
         </entry>
         <entry>
             <key>DATE_MODIFICATION</key>
-            <value>1626091122014</value>
+            <value>1626096826841</value>
         </entry>
         <entry>
             <key>REPOSITORY</key>
@@ -15,7 +15,7 @@
         </entry>
         <entry>
             <key>DATE_CREATION</key>
-            <value>1626091122009</value>
+            <value>1626096826832</value>
         </entry>
         <entry>
             <key>TAG</key>
--- Upgrade_MSA_/Process_Backup/Tasks/Task_backup.py
+++ Upgrade_MSA_/Process_Backup/Tasks/Task_backup.py
@@ -48,7 +48,7 @@ def ssh(host, cmd, user, password, timeout=30, bg_run=False):
 '''
 context = Variables.task_call(dev_var)
 '''
-ssh(host, "ls -al", username, password)
+ssh(context['host'], "cp "+str(context['quickstartDir'])+"/docker-compose.yml /tmp/docker-compose.yml", context['username'], context['password'])
 '''
 ret = MSA_API.process_content('ENDED', f'Backup successful ', context, True)
 print(ret)
