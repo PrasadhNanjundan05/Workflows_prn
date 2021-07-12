9dda60aefe734fc217c8aa302fc3fa393587d1cf
PrasadhNanjundan05 <>
2021-07-12 14:14:04 +0000

    Updating Workflow on  - 12-07-2021 14:14:04 - ncroot

--- Upgrade_MSA_/Process_Backup/Tasks/.meta_Task_backup.py
+++ Upgrade_MSA_/Process_Backup/Tasks/.meta_Task_backup.py
@@ -7,7 +7,7 @@
         </entry>
         <entry>
             <key>DATE_MODIFICATION</key>
-            <value>1626099217650</value>
+            <value>1626099244743</value>
         </entry>
         <entry>
             <key>REPOSITORY</key>
@@ -15,7 +15,7 @@
         </entry>
         <entry>
             <key>DATE_CREATION</key>
-            <value>1626099217646</value>
+            <value>1626099244740</value>
         </entry>
         <entry>
             <key>TAG</key>
--- Upgrade_MSA_/Process_Backup/Tasks/Task_backup.py
+++ Upgrade_MSA_/Process_Backup/Tasks/Task_backup.py
@@ -8,12 +8,12 @@ from msa_sdk.variables import Variables
 from msa_sdk.msa_api import MSA_API
 
 dev_var = Variables()
-'''
+
 dev_var.add('host', var_type='String')
 dev_var.add('username', var_type='String')
 dev_var.add('password', var_type='String')
 dev_var.add('quickstartDir', var_type='String')
-'''
+
 
 '''
 
