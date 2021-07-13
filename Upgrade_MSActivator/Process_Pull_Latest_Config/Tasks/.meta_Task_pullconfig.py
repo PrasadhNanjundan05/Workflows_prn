81d2d54e57e0f5539f717ce602408508571836ac
PrasadhNanjundan05 <>
2021-07-13 14:01:11 +0000

    Updating Workflow on  - 13-07-2021 14:01:11 - ncroot

--- Upgrade_MSActivator/Process_Pull_Latest_Config/Tasks/.meta_Task_pullconfig.py
+++ Upgrade_MSActivator/Process_Pull_Latest_Config/Tasks/.meta_Task_pullconfig.py
@@ -7,7 +7,7 @@
         </entry>
         <entry>
             <key>DATE_MODIFICATION</key>
-            <value>1626184855684</value>
+            <value>1626184871940</value>
         </entry>
         <entry>
             <key>REPOSITORY</key>
@@ -15,7 +15,7 @@
         </entry>
         <entry>
             <key>DATE_CREATION</key>
-            <value>1626184855681</value>
+            <value>1626184871936</value>
         </entry>
         <entry>
             <key>TAG</key>
