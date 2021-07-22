9ede5775bad60665d0180b3b34059e40dd089f8c
PrasadhNanjundan05 <>
2021-07-22 07:58:25 +0000

    Updating Workflow on  - 22-07-2021 07:58:25 - ncroot

--- MSCallTest/Process_Add/Tasks/.meta_static_route_CREATE.py
+++ MSCallTest/Process_Add/Tasks/.meta_static_route_CREATE.py
@@ -7,7 +7,7 @@
         </entry>
         <entry>
             <key>DATE_MODIFICATION</key>
-            <value>1626940662658</value>
+            <value>1626940705387</value>
         </entry>
         <entry>
             <key>REPOSITORY</key>
@@ -15,7 +15,7 @@
         </entry>
         <entry>
             <key>DATE_CREATION</key>
-            <value>1626940662555</value>
+            <value>1626940705383</value>
         </entry>
         <entry>
             <key>TAG</key>
--- MSCallTest/Process_Add/Tasks/static_route_CREATE.py
+++ MSCallTest/Process_Add/Tasks/static_route_CREATE.py
@@ -2,7 +2,7 @@ import json
 from msa_sdk.variables import Variables
 from msa_sdk.msa_api import MSA_API
 from msa_sdk.order import Order
-123
+
 # List all the parameters required by the task
 dev_var = Variables()
 dev_var.add('device_id', var_type='Device')
