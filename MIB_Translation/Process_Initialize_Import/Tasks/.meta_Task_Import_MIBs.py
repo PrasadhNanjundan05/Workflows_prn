2bf59c7de19d4f30e833f890d434fa914b9bc6f6
ncuser <ncuser@59fe128091b5>
2022-02-23 14:22:59 +0000

    Updating Workflow on  - 23-02-2022 14:22:59 - ncroot

--- MIB_Translation/Process_Initialize_Import/Tasks/.meta_Task_Import_MIBs.py
+++ MIB_Translation/Process_Initialize_Import/Tasks/.meta_Task_Import_MIBs.py
@@ -7,7 +7,7 @@
         </entry>
         <entry>
             <key>DATE_MODIFICATION</key>
-            <value>1645624383775</value>
+            <value>1645626179331</value>
         </entry>
         <entry>
             <key>REPOSITORY</key>
@@ -15,7 +15,7 @@
         </entry>
         <entry>
             <key>DATE_CREATION</key>
-            <value>1645624382453</value>
+            <value>1645626179329</value>
         </entry>
         <entry>
             <key>TAG</key>
--- MIB_Translation/Process_Initialize_Import/Tasks/Task_Import_MIBs.py
+++ MIB_Translation/Process_Initialize_Import/Tasks/Task_Import_MIBs.py
@@ -47,9 +47,10 @@ for mib_path in mibs_path_root_list:
 '''
 Parse the MIBs
     translate OID names in OIDs thanks to snmptranslate CLI
-    build the dictionary imported_oid_list with key = oid, value = oid name
+    build the dictionary oid_list with key = oid, value = oid name
+    to ensure unicity of the oid
 '''
-imported_oid_list = {}
+oid_list = {}
 mib_path_str = ':'.join(mibs_path_list)
 mib_name_str = ':'.join(mib_name_list)
 cmd = ['/usr/bin/snmptranslate', '-Pu', '-Tz', '-M', mib_path_str, '-m', mib_name_str]
@@ -64,8 +65,19 @@ if proc.returncode == 0:
     if result != None:
         # result is a list of tuple, each tuple is a (oid_name, oid) pair
         for name, oid in result:
-            if not oid in imported_oid_list:
-                imported_oid_list[oid] = name
+            if not oid in oid_list:
+                oid_list[oid] = name
+
+'''
+Build a new dictionary for the WF task
+'''
+imported_oid_list = {}
+i = 0
+for oid, name in oid_list.items():
+    imported_oid_list[i] = {}
+    imported_oid_list[i]['oid'] = oid
+    imported_oid_list[i]['oid_name'] = name
+    i += 1
 
 context['imported_oids'] = imported_oid_list
 
