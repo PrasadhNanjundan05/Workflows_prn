e92f240270ae8451e4c714a20f2e02ea753fac0c
ncuser <ncuser@93a473e3e3e3>
2022-02-25 18:16:47 +0000

    Updating Workflow on  - 25-02-2022 18:16:47 - ncroot

--- MIB_Translation/Process_Translate_OIDs/Tasks/.meta_Task_Translate.py
+++ MIB_Translation/Process_Translate_OIDs/Tasks/.meta_Task_Translate.py
@@ -7,7 +7,7 @@
         </entry>
         <entry>
             <key>DATE_MODIFICATION</key>
-            <value>1645811811286</value>
+            <value>1645813007348</value>
         </entry>
         <entry>
             <key>REPOSITORY</key>
@@ -15,7 +15,7 @@
         </entry>
         <entry>
             <key>DATE_CREATION</key>
-            <value>1645811811283</value>
+            <value>1645813007334</value>
         </entry>
         <entry>
             <key>TAG</key>
