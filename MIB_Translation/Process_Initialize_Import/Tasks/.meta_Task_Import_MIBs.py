0f0d8efbcce53ef4442226e56d71b83b77bc422d
ncuser <ncuser@93a473e3e3e3>
2022-02-28 10:10:44 +0000

    Updating Workflow on  - 28-02-2022 10:10:44 - ncroot

--- MIB_Translation/Process_Initialize_Import/Tasks/.meta_Task_Import_MIBs.py
+++ MIB_Translation/Process_Initialize_Import/Tasks/.meta_Task_Import_MIBs.py
@@ -7,7 +7,7 @@
         </entry>
         <entry>
             <key>DATE_MODIFICATION</key>
-            <value>1646042299699</value>
+            <value>1646043044830</value>
         </entry>
         <entry>
             <key>REPOSITORY</key>
@@ -15,7 +15,7 @@
         </entry>
         <entry>
             <key>DATE_CREATION</key>
-            <value>1646042299695</value>
+            <value>1646043044827</value>
         </entry>
         <entry>
             <key>TAG</key>
