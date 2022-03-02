447bb66011cd55635d77255f90d83fac0786b64a
ncuser <ncuser@93a473e3e3e3>
2022-03-02 14:31:34 +0000

    Updating Workflow on  - 02-03-2022 14:31:34 - ncroot

--- Test_boolean/Process_Create/Tasks/.meta_Task_create.py
+++ Test_boolean/Process_Create/Tasks/.meta_Task_create.py
@@ -7,7 +7,7 @@
         </entry>
         <entry>
             <key>DATE_MODIFICATION</key>
-            <value>1646231365884</value>
+            <value>1646231494882</value>
         </entry>
         <entry>
             <key>REPOSITORY</key>
@@ -15,7 +15,7 @@
         </entry>
         <entry>
             <key>DATE_CREATION</key>
-            <value>1646231365881</value>
+            <value>1646231494879</value>
         </entry>
         <entry>
             <key>TAG</key>
--- Test_boolean/Process_Create/Tasks/Task_create.py
+++ Test_boolean/Process_Create/Tasks/Task_create.py
@@ -4,6 +4,7 @@ from msa_sdk.msa_api import MSA_API
 dev_var = Variables()
 
 context = Variables.task_call(dev_var)
-ret = MSA_API.process_content('ENDED', 'Task OK', context, True)
+
+ret = MSA_API.process_content('ENDED', 'Create OK', context, True)
 print(ret)
 
