cb2d016409f38e9cc665d80db1019b25892991dd
ncuser <ncuser@93a473e3e3e3>
2022-03-02 14:29:25 +0000

    Updating Workflow on  - 02-03-2022 14:29:25 - ncroot

--- Test_boolean/Process_Create/Tasks/Task_create.py
+++ Test_boolean/Process_Create/Tasks/Task_create.py
@@ -2,7 +2,6 @@ from msa_sdk.variables import Variables
 from msa_sdk.msa_api import MSA_API
 
 dev_var = Variables()
-dev_var.add('array.0.select', var_type='Boolean')
 
 context = Variables.task_call(dev_var)
 ret = MSA_API.process_content('ENDED', 'Task OK', context, True)
