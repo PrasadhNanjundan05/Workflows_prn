14be08ae853c1883d86b8223b6bb2751c97224a2
ncuser <ncuser@ab2cbb6e355c>
2022-04-22 09:37:30 +0000

    Updating Workflow on  - 22-04-2022 09:37:30 - ncroot

--- Failover/Process_Initialize/Tasks/Task_Initialize.py
+++ Failover/Process_Initialize/Tasks/Task_Initialize.py
@@ -3,10 +3,11 @@ from msa_sdk.msa_api import MSA_API
 
 dev_var = Variables()
 dev_var.add('device_id', var_type='Device')
-dev_var.add('Failover.0.object_id', var_type='String')
+dev_var.add('Failover.object_id', var_type='String')
 
 context = Variables.task_call(dev_var)
-context['Failover']['0']['object_id'] = 'ip'
+context['Failover'] = []
+context['Failover']['object_id'] = 'ip'
 
 ret = MSA_API.process_content('ENDED', 'Task OK', context, True)
 print(ret)
