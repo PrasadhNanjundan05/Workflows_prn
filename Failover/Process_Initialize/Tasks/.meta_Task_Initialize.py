abb251f8252d32ea860454262ffc164b1f3421a1
ncuser <ncuser@ab2cbb6e355c>
2022-04-22 09:31:13 +0000

    Updating Workflow on  - 22-04-2022 09:31:13 - ncroot

--- Failover/Process_Initialize/Tasks/.meta_Task_Initialize.py
+++ Failover/Process_Initialize/Tasks/.meta_Task_Initialize.py
@@ -7,7 +7,7 @@
         </entry>
         <entry>
             <key>DATE_MODIFICATION</key>
-            <value>1650619313479</value>
+            <value>1650619873188</value>
         </entry>
         <entry>
             <key>REPOSITORY</key>
@@ -15,7 +15,7 @@
         </entry>
         <entry>
             <key>DATE_CREATION</key>
-            <value>1650619313476</value>
+            <value>1650619873172</value>
         </entry>
         <entry>
             <key>TAG</key>
--- Failover/Process_Initialize/Tasks/Task_Initialize.py
+++ Failover/Process_Initialize/Tasks/Task_Initialize.py
@@ -3,6 +3,7 @@ from msa_sdk.msa_api import MSA_API
 
 dev_var = Variables()
 dev_var.add('device_id', var_type='Device')
+dev_var.add('Failover.0.object_id', var_type='String')
 
 context = Variables.task_call(dev_var)
 
