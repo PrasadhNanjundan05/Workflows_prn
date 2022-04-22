ed897689dcb86836d350e8dad19ff692a4758a84
ncuser <ncuser@ab2cbb6e355c>
2022-04-22 13:24:59 +0000

    Updating Workflow on  - 22-04-2022 13:24:59 - ncroot

--- Failover/Process_Activate_SIX1/Tasks/Task_Backup_configuration.py
+++ Failover/Process_Activate_SIX1/Tasks/Task_Backup_configuration.py
@@ -3,7 +3,6 @@ from msa_sdk.msa_api import MSA_API
 from msa_sdk.conf_backup import ConfBackup
 
 dev_var = Variables()
-#dev_var.add('device_id', var_type='Device')
 
 context = Variables.task_call(dev_var)
 
@@ -16,24 +15,6 @@ result = 'RUNNING'
 while result == 'RUNNING':
     result = conf_backup.backup_status(devicelongid)
 
-'''
-Format of the Task response :
-JSON format : {"wo_status":"status","wo_comment":"comment","wo_newparams":{json_body}}
-wo_status : ENDED [Green color] or FAILED [Red color] or WARNING [Orange color]
-			-> While the Task is Running [means no response returned yet], task status is RUNNING [Blue color]
-         -> When status is returned as FAILED, the Orchestration Engine stops the Process Execution from this Task
-wo_comment : Appropriate Comment to display as per the success/failure of the Task
-wo_newparams : json_body parameters returned from this Task
-
-Function process_content() takes care of Creating a Json response from inputs
-This function definiton can be found at : http://[YOUR_MSA_URL]/msa_sdk/msa_api.html#msa_sdk.msa_api.MSA_API.process_content
-NOTE : For 'wo_newparams', always pass "context" [whether wo_status is ENDED/FAILED/WARNING to preserve it across Service Instance]
-    -> Last argument "true" mentions whether the json_response to be Logged in the logfile : /opt/jboss/latest/logs/process.log
-    -> If not passed, it's "false"
-
-The response "ret" should be echoed from the Task "print(ret)" which is read by Orchestration Engine
-In case of FAILURE/WARNING, the Task can be Terminated by calling "exit" as per Logic
-'''
 if result == 'ENDED':
     ret = MSA_API.process_content('ENDED', 'Backup done', context, True)
 else:
