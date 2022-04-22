fc7585c64c454cdbc650c05656284129e254eb96
ncuser <ncuser@ab2cbb6e355c>
2022-04-22 15:24:20 +0000

    Updating Workflow on  - 22-04-2022 15:24:20 - ncroot

--- Failover/Process_Initialize/Tasks/.meta_Failover_IMPORT.py
+++ Failover/Process_Initialize/Tasks/.meta_Failover_IMPORT.py
@@ -7,7 +7,7 @@
         </entry>
         <entry>
             <key>DATE_MODIFICATION</key>
-            <value>1650641029857</value>
+            <value>1650641060163</value>
         </entry>
         <entry>
             <key>REPOSITORY</key>
@@ -15,7 +15,7 @@
         </entry>
         <entry>
             <key>DATE_CREATION</key>
-            <value>1650641029840</value>
+            <value>1650641060160</value>
         </entry>
         <entry>
             <key>TAG</key>
--- Failover/Process_Initialize/Tasks/Failover_IMPORT.py
+++ Failover/Process_Initialize/Tasks/Failover_IMPORT.py
@@ -2,7 +2,7 @@ import json
 from msa_sdk.variables import Variables
 from msa_sdk.msa_api import MSA_API
 from msa_sdk.order import Order
-from ma_sdk.orchestration import Orchestration
+from msa_sdk.orchestration import Orchestration
 
 # List all the parameters required by the task
 dev_var = Variables()
