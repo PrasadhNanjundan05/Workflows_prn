bf7345c5b01facb2b80205da0e33b235f79a9afa
ncuser <ncuser@a4185141896c>
2023-02-06 16:56:26 +0000

    Updating Workflow on  - 06-02-2023 16:56:26 - ncroot

--- WF_for_test_MS_ydu/Process_Add/Tasks/.meta_test_MS_ydu_CREATE.py
+++ WF_for_test_MS_ydu/Process_Add/Tasks/.meta_test_MS_ydu_CREATE.py
@@ -7,7 +7,7 @@
         </entry>
         <entry>
             <key>DATE_MODIFICATION</key>
-            <value>1675702579470</value>
+            <value>1675702586391</value>
         </entry>
         <entry>
             <key>REPOSITORY</key>
@@ -15,7 +15,7 @@
         </entry>
         <entry>
             <key>DATE_CREATION</key>
-            <value>1675702579461</value>
+            <value>1675702586385</value>
         </entry>
         <entry>
             <key>TAG</key>
--- WF_for_test_MS_ydu/Process_Add/Tasks/test_MS_ydu_CREATE.py
+++ WF_for_test_MS_ydu/Process_Add/Tasks/test_MS_ydu_CREATE.py
@@ -2,7 +2,6 @@ import json
 from msa_sdk.variables import Variables
 from msa_sdk.msa_api import MSA_API
 from msa_sdk.order import Order
-from msa_sdk import util
 
 # List all the parameters required by the task
 dev_var = Variables()
