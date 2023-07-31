42322fa906ca26681d681bd5136fd666a09b1fc3
ncuser <ncuser@7a1588698b76>
2023-07-31 10:00:50 +0000

    Updating Workflow on  - 31-07-2023 10:00:50 - ncroot

---
+++ Service_Ordering/Process_Create_Service_Order/Tasks/.meta_Task_Service_create_Task.py
@@ -0,0 +1,35 @@
+<?xml version="1.0" encoding="UTF-8" standalone="yes"?>
+<metadata>
+    <map>
+        <entry>
+            <key>DISPLAYNAME</key>
+            <value>Task_Service_create_Task.py</value>
+        </entry>
+        <entry>
+            <key>DATE_MODIFICATION</key>
+            <value>1690797650089</value>
+        </entry>
+        <entry>
+            <key>REPOSITORY</key>
+            <value>Process</value>
+        </entry>
+        <entry>
+            <key>DATE_CREATION</key>
+            <value>1690797650084</value>
+        </entry>
+        <entry>
+            <key>TAG</key>
+        </entry>
+        <entry>
+            <key>FILE_TYPE</key>
+            <value>text</value>
+        </entry>
+        <entry>
+            <key>TYPE</key>
+            <value>UPLOAD</value>
+        </entry>
+        <entry>
+            <key>COMMENT</key>
+        </entry>
+    </map>
+</metadata>
