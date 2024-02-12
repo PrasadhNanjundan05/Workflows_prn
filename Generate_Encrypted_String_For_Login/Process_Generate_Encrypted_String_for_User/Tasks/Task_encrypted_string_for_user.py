460b0d3a9a61eaf60723aaa9b2277555db064cc1
ncroot <>
2024-02-12 14:13:04 +0000

    Updating Workflow on  - 12-02-2024 14:13:04 - ncroot

--- Generate_Encrypted_String_For_Login/Process_Generate_Encrypted_String_for_User/Tasks/Task_encrypted_string_for_user.py
+++ Generate_Encrypted_String_For_Login/Process_Generate_Encrypted_String_for_User/Tasks/Task_encrypted_string_for_user.py
@@ -1,54 +1,52 @@
-'''
-Visit http://[YOUR_MSA_URL]/msa_sdk/ to see what you can import.
-'''
+import sys
+import warnings
+
+if not sys.warnoptions:
+    warnings.simplefilter("ignore")
+
 from msa_sdk.variables import Variables
 from msa_sdk.msa_api import MSA_API
+from cryptography.hazmat.primitives.ciphers import Cipher, algorithms, modes
+from cryptography.hazmat.backends import default_backend
+from cryptography.hazmat.primitives import serialization, hashes
+from cryptography.hazmat.primitives.kdf.pbkdf2 import PBKDF2HMAC
+import base64
 
-'''
-List all the parameters required by the task
 
-You can use var_name convention for your variables
-They will display automaticaly as "Var Name"
-The allowed types are:
-  'String', 'Boolean', 'Integer', 'Password', 'IpAddress',
-  'IpMask', 'Ipv6Address', 'Composite', 'OBMFRef', 'Device'
-
- Add as many variables as needed
-'''
 dev_var = Variables()
-dev_var.add('var_name', var_type='String')
-dev_var.add('var_name2', var_type='Integer')
+dev_var.add("username",type="String")
+dev_var.add("password",type="Password")
 
-'''
-context => Service Context variable per Service Instance
-All the user-inputs of Tasks are automatically stored in context
-Also, any new variables should be stored in context which are used across Service Instance
-The variables stored in context can be used across all the Tasks and Processes of a particular Service
-Update context array [add/update/delete variables] as per requirement
 
-ENTER YOUR CODE HERE
-'''
 context = Variables.task_call(dev_var)
-context['var_name2'] = int(context['var_name2']) + 1
 
-'''
-Format of the Task response :
-JSON format : {"wo_status":"status","wo_comment":"comment","wo_newparams":{json_body}}
-wo_status : ENDED [Green color] or FAILED [Red color] or WARNING [Orange color]
-			-> While the Task is Running [means no response returned yet], task status is RUNNING [Blue color]
-         -> When status is returned as FAILED, the Orchestration Engine stops the Process Execution from this Task
-wo_comment : Appropriate Comment to display as per the success/failure of the Task
-wo_newparams : json_body parameters returned from this Task
+def encrypt(username, password, shared_key):
+    data = f"{username}:{password}".encode("utf-8")
 
-Function process_content() takes care of Creating a Json response from inputs
-This function definiton can be found at : http://[YOUR_MSA_URL]/msa_sdk/msa_api.html#msa_sdk.msa_api.MSA_API.process_content
-NOTE : For 'wo_newparams', always pass "context" [whether wo_status is ENDED/FAILED/WARNING to preserve it across Service Instance]
-    -> Last argument "true" mentions whether the json_response to be Logged in the logfile : /opt/jboss/latest/logs/process.log
-    -> If not passed, it's "false"
+    # Generate Initialization Vector (IV)
+    iv = b'\x00' * 16  # For simplicity, all zeros IV is used
 
-The response "ret" should be echoed from the Task "print(ret)" which is read by Orchestration Engine
-In case of FAILURE/WARNING, the Task can be Terminated by calling "exit" as per Logic
-'''
+    # Convert the shared key to a SecretKeySpec
+    secret_key = base64.b64decode(shared_key)
+
+    # Create Cipher instance
+    cipher = Cipher(algorithms.AES(secret_key), modes.CBC(iv), backend=default_backend())
+    encryptor = cipher.encryptor()
+
+    # Encrypt the data
+    encrypted_bytes = encryptor.update(data) + encryptor.finalize()
+
+    # Base64 encode the encrypted data
+    return base64.b64encode(encrypted_bytes).decode("utf-8")
+
+
+try:
+    encrypted_data = encrypt(context.get("username"), context.get("password"), context.get("secret_key"))
+    context["encrypted_string_for_user"+str(context.get("username"))] = encrypted_data
+except Exception as e:
+    print(e)
+
+
 ret = MSA_API.process_content('ENDED', 'Task OK', context, True)
 print(ret)
 
