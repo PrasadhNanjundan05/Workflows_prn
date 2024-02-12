44aa8f0a99d853967c0a0ecdf33e401d2510e7c6
ncroot <>
2024-02-12 14:51:38 +0000

    Updating Workflow on  - 12-02-2024 14:51:38 - ncroot

--- Generate_Encrypted_String_For_Login/Process_Generate_Encrypted_String_for_User/Tasks/Task_encrypted_string_for_user.py
+++ Generate_Encrypted_String_For_Login/Process_Generate_Encrypted_String_for_User/Tasks/Task_encrypted_string_for_user.py
@@ -49,7 +49,6 @@ try:
 except Exception as e:
     print(e)
 
-context["TOKEN"] = "";
 ret = MSA_API.process_content('ENDED', 'Task OK', context, True)
 util.log_to_process_file(service_id, str(ret), process_id)
 print(ret)
