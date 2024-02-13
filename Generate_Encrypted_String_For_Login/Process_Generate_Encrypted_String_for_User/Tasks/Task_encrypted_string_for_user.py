5d8520a49c95cc791c43cd3a9dce706c6fb792e2
ncroot <>
2024-02-13 14:35:24 +0000

    Updating Workflow on  - 13-02-2024 14:35:24 - ncroot

--- Generate_Encrypted_String_For_Login/Process_Generate_Encrypted_String_for_User/Tasks/Task_encrypted_string_for_user.py
+++ Generate_Encrypted_String_For_Login/Process_Generate_Encrypted_String_for_User/Tasks/Task_encrypted_string_for_user.py
@@ -18,31 +18,19 @@ context = Variables.task_call(dev_var)
 service_id = context['SERVICEINSTANCEID']
 process_id = context['PROCESSINSTANCEID']
 
-import base64
-from hashlib import sha256
-from hmac import HMAC
-from Crypto.Util.Padding import pad
 from Crypto.Cipher import AES
+from Crypto.Util.Padding import pad
+from base64 import b64encode
 from Crypto.Random import get_random_bytes
 
-def encrypt(username, password, sharedKey):
+def encrypt(username, password, shared_key):
     data = username + ":" + password
+    iv = get_random_bytes(16)
     
-    # Generate Initialization Vector (IV)
-    iv = get_random_bytes(16)  # Random IV
+    cipher = AES.new(shared_key.encode(), AES.MODE_CBC, iv)
+    encrypted_bytes = cipher.encrypt(pad(data.encode('utf-8'), AES.block_size))
     
-    # Derive key from sharedKey using HMAC
-    key = HMAC(sharedKey.encode('utf-8'), iv, sha256).digest()
-    
-    # Create AES cipher object
-    cipher = AES.new(key, AES.MODE_CBC, iv)
-    
-    # Encrypt the data
-    encrypted_data = cipher.encrypt(pad(data.encode('utf-8'), AES.block_size))
-    
-    # Combine IV and encrypted data and Base64 encode
-    encrypted_bytes = iv + encrypted_data
-    return base64.b64encode(encrypted_bytes).decode('utf-8')
+    return b64encode(iv + encrypted_bytes).decode('utf-8')
 
 
 try:
